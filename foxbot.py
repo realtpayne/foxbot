99628bc50c411de62c130a72b82a8c26f3e59174
realtpayne <91302786+realtpayne@users.noreply.github.com>
2023-08-23 16:49:16 -0500

    Update foxbot.py
    fixed bad logic in skid event and automod

--- foxbot.py
+++ foxbot.py
@@ -14,7 +14,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bad_words = os.getenv('BAD_WORDS').split(' ') # reads BAD_WORDS from the .env file, stores a list of no no words
 
 intents = discord.Intents.all() #sets intents variable
-bot = commands.Bot(command_prefix=".", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -27,31 +27,29 @@ async def on_message(message):
             return
         # prevents bot from responding to itself
 
-        #PING = re.compile("ping", re.IGNORECASE)
-        #if PING.search(message.content):
-        #        await message.reply("Pong!")
-        # responds w/ pong if someone says ping
-        #replaced with command "ping" below
+        Skid = re.compile(r"\bfbi\b", re.IGNORECASE)
+        if (Skid.search(message.content) and message.author.id == 0000000000000000) : # fixed bad logic with spooky message for skid routine
+                await message.reply("We're already watching you.")
+        #responds with a spooky message if a specific user uses the phrase "fbi"
 
-        if any(x in message.content.lower() for x in bad_words):
-            await message.delete()
-
-#bot.event
-#async def on_message(message):
-#    if message.author.id == 00000000000000:
-#        await message.reply("We are not affiliated, nor do we condone the words coming out of this mans mouth")
-# sends a silly "we do not condone" message whenever a specific user sends messages
+        if any(re.search(r'\b{}\b'.format(x), message.content.lower()) for x in bad_words): # previous automod attempt had bad logic, causing words that contained bits of the "bad_words" variable to be filtered
+                        await message.delete()
 
 @bot.command()
 async def whoami(ctx, *args):
     if ctx.message.author.guild_permissions.administrator:
         await ctx.send(f"You should know, you're the one in charge {ctx.message.author.mention}")
-#    elif ctx.message.author.id == 000000000000: # added as a gag for a friend, responds with forklift certified
-#        await ctx.send(f"I know you, you're forklift certified") 
+    elif ctx.message.author.id == 000000000000000000:
+        await ctx.send(f"I know you, you're forklift certified")
     else:
         await ctx.send(f"You're {ctx.message.author}, one of my valued employees!")
 
     #.whomai returns if user is admin, or returns their name if not
+
+@bot.command()
+async def whoismcneth(ctx):
+        await ctx.send("Been a long time since I heard of that Busta.")
+        await ctx.send("https://i.ytimg.com/vi/GHQbQeP7XSU/maxresdefault.jpg")
 
 @bot.command()
 async def tableflip(ctx):
@@ -88,14 +86,14 @@ async def addquote(ctx, quote_):
             with open(file, "w+") as wp:
                 wp.write(json.dumps(j))
     try:
-        with open(file_path, "r"): # opens the quotes.json file
+        with open(file_path, "r"):
             pass
     except:
-        with open(file_path, "w+") as wp: #creates the file if can't be opened
-            wp.write('{"quotes" : []}') # json formatting
+        with open(file_path, "w+") as wp:
+            wp.write('{"quotes" : []}')
     finally:
-        add_quote(quote_) # adds the quote passed above
-        await ctx.send("Done!") 
+        add_quote(quote_)
+        await ctx.send("Done!")
 
 
 bot.run(TOKEN)
