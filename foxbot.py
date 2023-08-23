e069315e8c3b9a773a8024b9abc33d34efd6a496
realtpayne <91302786+realtpayne@users.noreply.github.com>
2023-08-23 14:56:06 -0500

    Update foxbot.py
    added quotes function w/ logic to ensure quotes.json file is queried/written from the working directory.

--- foxbot.py
+++ foxbot.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 from dotenv import load_dotenv #package for .env file
 import re # imports regex
 import requests
+import random
+import json
 
 load_dotenv() #loads the .env file
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -36,7 +38,7 @@ async def on_message(message):
 
 #bot.event
 #async def on_message(message):
-#    if message.author.id == insert_userid_integer_here:
+#    if message.author.id == 273274151085539339:
 #        await message.reply("We are not affiliated, nor do we condone the words coming out of this mans mouth")
 # sends a silly "we do not condone" message whenever a specific user sends messages
 
@@ -44,8 +46,11 @@ async def on_message(message):
 async def whoami(ctx, *args):
     if ctx.message.author.guild_permissions.administrator:
         await ctx.send(f"You should know, you're the one in charge {ctx.message.author.mention}")
+#    elif ctx.message.author.id == 000000000000: # added as a gag for a friend, responds with forklift certified
+#        await ctx.send(f"I know you, you're forklift certified") 
     else:
         await ctx.send(f"You're {ctx.message.author}, one of my valued employees!")
+
     #.whomai returns if user is admin, or returns their name if not
 
 @bot.command()
@@ -57,5 +62,40 @@ async def tableflip(ctx):
 async def ping(ctx):
         await ctx.send("pong")
         #pong
+
+@bot.command()
+async def quote(ctx):
+    script_dir = os.path.dirname(os.path.abspath(__file__)) # grabs the working directory from env
+    file_path = os.path.join(script_dir, "quotes.json") # ensures file edited is /path/to/workingdir/quotes.json
+    try:
+        with open(file_path, "r") as r:
+            j = json.load(r)
+            all_quotes = j["quotes"]
+    except:
+        await ctx.send("No quotes are stored! add it using the quotes command") # respond with no quotes if no quotes are added
+        return
+
+    await ctx.send(random.choice(all_quotes))
+
+@bot.command()
+async def addquote(ctx, quote_):
+    script_dir = os.path.dirname(os.path.abspath(__file__)) # same as variable for quote ctx
+    file_path = os.path.join(script_dir, "quotes.json")
+    def add_quote(quote, file=file_path):
+        with open(file, "r+") as fh:
+            j = json.load(fh)
+            j["quotes"].append(quote)
+            with open(file, "w+") as wp:
+                wp.write(json.dumps(j))
+    try:
+        with open(file_path, "r"): # opens the quotes.json file
+            pass
+    except:
+        with open(file_path, "w+") as wp: #creates the file if can't be opened
+            wp.write('{"quotes" : []}') # json formatting
+    finally:
+        add_quote(quote_) # adds the quote passed above
+        await ctx.send("Done!") 
+
 
 bot.run(TOKEN)
